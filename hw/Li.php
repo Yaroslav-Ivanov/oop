df3c15a6045dfea2eaf0b74a76d72ef58960e900
Yaroslav-Ivanov <yaroslavvitebsk@gmail.com>
2020-04-24 18:44:03 +0300

    .

---
+++ hw/Li.php
@@ -0,0 +1,12 @@
+<?php
+class Li extends Tag
+{
+    protected $data;
+
+    public function setInnerData(array $data){
+        $this->data = "";
+        foreach($data as $value){
+            $this->data .= "<li>$value</li>";    
+        }
+ }  
+}
---
+++ hw/UL.php
@@ -0,0 +1,9 @@
+<?php
+
+class UL extends Li
+{
+    public function html()
+    {
+        return "\n<ul $this->style $this->class $this->id>\n$this->data</ul> ";
+    }
+}
